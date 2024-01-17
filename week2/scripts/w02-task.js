ba097f3ae07514d250316ae52e9e95fbf8015f96
jasonrao <jasonrao@byui.edu>
2024-01-16 21:24:17 -0700

    week2

---
+++ week2/scripts/w02-task.js
@@ -0,0 +1,45 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = 'Yujie Rao';
+const date = new Date();
+const currentYear = date.getFullYear();
+const profilePicture = 'images/IMG_0123-3.jpg';
+
+
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
+
+
+
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = '<strong> ${fullName} </strong>';
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src',profilePicture);
+imageElement.setAttribute('alt','Profile Image of ${fullName}');
+
+
+
+
+
+
+
+/* Step 5 - Array */
+let list = ['Pizza','Sushi','Tacos','Chocolate Cake', 'Spinach Salad'];
+foodElement.textContent = list;
+let list2 = ['Chicken Curry','Ice Cream'];
+list.push(list2);
+foodElement.innerHTML += '<br>${favFoods}';
+
+
+
+
+
