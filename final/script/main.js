e2ef6e2ce5c4dc109fb945dc0e099e7db53caf9b
jasonrao <jasonrao@byui.edu>
2024-02-12 22:16:15 -0700

    display cameras

--- final/script/main.js
+++ final/script/main.js
@@ -1,26 +1,26 @@
-const camerasElement = document.querySelector("#cameras");
+const camerasElement = document.querySelector("#camera");
 let cameraList = {};
 
-/* async displayTemples Function */
-const displayTemples = (cameraList) => {
+/* async displayCameras Function */
+const displayCameras = (cameraList) => {
     cameraList.forEach((camera) => {
         const article = document.createElement("article");
         const h3 = document.createElement("h3");
         h3.textContent = camera.cameraName;
         const img = document.createElement("img");
         img.src = camera.imageUrl;
-        img.alt = "location";
+        img.alt = "brand";
         article.appendChild(h3);
         article.appendChild(img);
         camerasElement.appendChild(article);
     });
 };
 
-/* async getTemples Function using fetch()*/
-const getTemples = async () => {
+/* async getCameras Function using fetch()*/
+const getCameras = async () => {
     const response = await fetch("https://ryjason.github.io/cse121b/finalapi.json");
     cameraList = await response.json();
-    displayTemples(cameraList);
+    displayCameras(cameraList);
 };
 
 /* reset Function */
@@ -28,22 +28,22 @@ const reset = () => {
     camerasElement.innerHTML = "";
 };
 
-/* filterTemples Function */
-const filterTemples = (cameras) => {
+/* filterCameras Function */
+const filterCameras = (cameras) => {
     reset();
     const filter = document.getElementById("filtered").value;
     switch (filter) {
         case "utah":
-            displayTemples(cameras.filter(camera => camera.location.toLowerCase().includes("utah")));
+            displayCameras(cameras.filter(camera => camera.location.toLowerCase().includes("utah")));
             break;
         case "notutah":
-            displayTemples(cameras.filter(camera => !camera.location.toLowerCase().includes("utah")));
+            displayCameras(cameras.filter(camera => !camera.location.toLowerCase().includes("utah")));
             break;
         case "older":
-            displayTemples(cameras.filter(camera => new Date(camera.dedicated) < new Date(1950, 0, 1)));
+            displayCameras(cameras.filter(camera => new Date(camera.dedicated) < new Date(1950, 0, 1)));
             break;
         case "all":
-            displayTemples(cameras);
+            displayCameras(cameras);
             break;
         default:
             break;
@@ -51,5 +51,5 @@ const filterTemples = (cameras) => {
 };
 
 /* Event Listener */
-document.querySelector("#filtered").addEventListener("change", () => { filterTemples(cameraList) });
-getTemples();
+document.querySelector("#filtered").addEventListener("change", () => { filterCameras(cameraList) });
+getCameras();
